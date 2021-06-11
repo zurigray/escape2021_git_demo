6a0250672129791f7e55432e2877f100d23fb90e
Zuri Gray <zuri.gray@armagh.ac.uk>
2021-06-11 16:01:47 +0100

    Add fibonacci generator

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,4 @@
-from functools import cache
 
-
-@cache
 def fibonacci(n):
 	if n ==0:
 		return 0
@@ -9,6 +6,9 @@ def fibonacci(n):
 		return 1
 	return fibonacci(n-1) + fibonacci(n-2)
 
+def fib_gen(n):
+	for i in range(n):
+		yield fibonacci(n)
 
 if __name__ == '__main__':
 	print(fibonacci(35))
